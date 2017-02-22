5bf61596b32c95ecdc67c23a9087eac435f96bfa
KevinHutinPolytech <hutinkevin@yahoo.fr>
2017-02-22 19:51:00 +0100

    Add files via upload

--- json2redis.py
+++ json2redis.py
@@ -1,19 +1,22 @@
 import redis
 import sys
+import re
 
 basejson = redis.StrictRedis(host='127.0.0.1',port=6379,db=0)
 
 filename = sys.argv[1]
 
 with open(filename, 'r') as f:
-    try :
-        tweet = json.loads(line)
-        print('tweet')
-        date = json.dumps(tweet['created_at'])
-        basejson.hset(filename,date,tweet)
-    except:
-        print('failed try')
-        pass
+    m = re.search(".",line) # Permet D'éviter le bug lorsqu'il y a un saut de ligne    
+        if m != None :
+            try :
+                tweet = json.loads(line)
+                print('tweet')
+                date = json.dumps(tweet['created_at'])
+                basejson.hset(filename,date,tweet)
+            except:
+                print('failed try')
+                pass
 print(basejson.hscan(filename))
 
 
