5bf1bd526a9c5e139a08a54d4117710b31e0b848
KevinHutinPolytech <hutinkevin@yahoo.fr>
2017-02-22 19:54:24 +0100

    Add files via upload

--- json2redis.py
+++ json2redis.py
@@ -1,9 +1,13 @@
+# coding: utf8
+from __future__ import unicode_literals
+
 import redis
 import sys
 import re
 
 basejson = redis.StrictRedis(host='127.0.0.1',port=6379,db=0)
 
+print(sys.argv[1])
 filename = sys.argv[1]
 
 with open(filename, 'r') as f:
