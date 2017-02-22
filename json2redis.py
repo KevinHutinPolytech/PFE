407c245168f4b461c505ebf7b6838ccb4852c800
KevinHutinPolytech <hutinkevin@yahoo.fr>
2017-02-22 20:04:19 +0100

    Add files via upload

--- json2redis.py
+++ json2redis.py
@@ -4,19 +4,19 @@ from __future__ import unicode_literals
 import redis
 import sys
 import re
+import json
 
 basejson = redis.StrictRedis(host='127.0.0.1',port=6379,db=0)
 
 print(sys.argv[1])
 filename = sys.argv[1]
-
+#filename = 'stream__Macron.json'
 with open(filename, 'r') as f:
     for line in f:
         m = re.search(".",line) # Permet D'éviter le bug lorsqu'il y a un saut de ligne    
         if m != None :
-            try :
+            try :                
                 tweet = json.loads(line)
-                print('tweet')
                 date = json.dumps(tweet['created_at'])
                 basejson.hset(filename,date,tweet)
             except:
