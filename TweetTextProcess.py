c2cd5e5dde8e4c8ffe4c7366646eb910e6a4c798
KevinHutinPolytech <hutinkevin@yahoo.fr>
2017-02-24 11:38:05 +0100

    Add files via upload

---
+++ TweetTextProcess.py
@@ -0,0 +1,80 @@
+# coding: utf8
+from __future__ import unicode_literals
+####################################################################################
+# Traiter les data tokenisé
+####################################################################################
+
+#
+# Dans un premier temps on remani le tokeniser afin qu'il prenne en compte les emoticons, les RT, les @-mention, les url, ...
+#
+import operator 
+import json
+from collections import Counter
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+import redis
+from nltk.corpus import stopwords
+import string
+from nltk import bigrams 
+
+
+def tokenize(s):
+    return tokens_re.findall(s)
+ 
+def preprocess(s, lowercase=True):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+
+def getTweetsByHash(filename,database): 
+    listOfTweets = database.hvals(filename)
+    return listOfTweets
+
+def getTweetText(TweetStr):
+    tweetDict = json.loads(TweetStr)
+    tweetText = tweetDict['text']
+    return tweetText
+
+#####TEST####
+database = redis.StrictRedis(host='127.0.0.1',port=6379,db=0)
+listOfTweets = getTweetsByHash("presidentielle.json",database)
+tweetText = getTweetText(listOfTweets[0])
+print(tweetText)
+#############
+##
+##emoticons_str = r"""
+##    (?:
+##        [:=;] # Eyes
+##        [oO\-]? # Nose (optional)
+##        [D\)\]\(\]/\\OpP] # Mouth
+##    )"""
+## 
+##regex_str = [
+##    emoticons_str,
+##    r'<[^>]+>', # HTML tags
+##    r'(?:@[\w_]+)', # @-mentions
+##    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+##    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+## 
+##    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+##    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+##    r'(?:[\w_]+)', # other words
+##    r'(?:\S)', # anything else
+##   # r'([a-z0-9]+(\\u)[a-z0-9]+)'
+##]
+##    
+##tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+##emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+##
+##"""
+##The regular expressions are compiled with the flags re.VERBOSE, to allow spaces in the regexp to be ignored (see the multi-line emoticons regexp),
+##and re.IGNORECASE to catch both upper and lowercases. The tokenize() function simply catches all the tokens in a string and returns them as a list.
+##This function is used within preprocess(), which is used as a pre-processing chain: in this case we simply add a lowercasing feature for all the
+##tokens that are not emoticons (e.g. :D doesn’t become :d).
+##"""
+##
+##punctuation = list(string.punctuation)
+##stop = stopwords.words('french') + punctuation + ['via','le','les','a'] # Liste des tokens à effacer
--- json2redis.py
+++ json2redis.py
@@ -10,76 +10,25 @@ basejson = redis.StrictRedis(host='127.0.0.1',port=6379,db=0)
 
 print(sys.argv[1])
 filename = sys.argv[1]
-#filename = 'stream__Macron.json'
 
-#### VERSION 1
 count = 0
-with open("presidentielle.json", 'r') as f:
+with open(filename, 'r') as f:
     for line in f:
         m = re.search(".",line) # Permet D'éviter le bug lorsqu'il y a un saut de ligne    
         if m != None :
             try :
-                count = count + 1
-                if (count <= 3) :
-                    print(count)
-                    tweet = json.loads(line)
-                    print(type(tweet))
-                    print(tweet)
-                    print(tweet.keys())
-                    print(tweet['id_str'])
-                    print(tweet['text'])                    
-                    
-                   # tweetid = json.dumps(tweet('id'))
-                   # basejson.hset(filename,count,tweet)
-                   # tweet = json.loads(line)
-                   # date = json.dumps(tweet['created_at'])
-                    print('ok')
+                '''count = count + 1
+                print(count)
+                print(type(tweet))
+                print(tweet)
+                print(tweet.keys())
+                print(tweet['id_str'])
+                print(tweet['text'])                    
+                print('ok')'''
+                tweet = json.loads(line)
+                basejson.hset(filename,tweet['id_str'],json.dumps(tweet))
             except:
                 print('failed try')
                 pass
-            basejson.hset(filename,tweet['id_str'],json.dumps(tweet))
-#print(basejson.hscan(file))
+            
 
-listTweet = basejson.hvals(filename)
-print(listTweet[1])
-print(type(listTweet[1]))
-print(type(json.loads(listTweet[1])))
-tweetdict = json.loads(listTweet[1])
-print(tweetdict['text'])
-
-##
-##
-##print(file)
-##print('/n')
-##print('/n')
-##
-##print(file[1])
-##print('/n')
-##print('/n')
-##
-##
-##print(json.dumps(tweet['text']))
-##
-##basejson.set(filename,file)
-##print(basejson.get(filename))
-##'''
-###### Version 2
-##f=open(filename,'r')
-##s=f.read()
-##pyObject = json.loads(s)
-##basejson.set(filename,pyObject)
-##'''
-##file = basejson.get(filename)
-##print(type(file))
-##tweet = json.loads(file)
-##print(tweet)
-##print(type(tweet))
-##print(json.dumps(tweet['text']))
-##
-##for line in file:
-##    print('line')
-##    print (line)
-##    tweetText= json.dumps(line['text'])
-##    print('tweetText')
-##    print(tweetText)
-##    
