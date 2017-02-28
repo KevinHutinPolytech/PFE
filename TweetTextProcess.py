5e88aaae7648fc6f5c1bcab39f8eb9998b5f74aa
KevinHutinPolytech <hutinkevin@yahoo.fr>
2017-02-28 15:25:01 +0100

    Add files via upload

--- TweetTextProcess.py
+++ TweetTextProcess.py
@@ -20,6 +20,7 @@ import string
 from nltk import bigrams 
 from unidecode import unidecode
 from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.snowball import FrenchStemmer
 
 def tokenize(s):
     return tokens_re.findall(s)
@@ -86,10 +87,12 @@ listOfTweets = getTweetsByHash(cle,database)
 punctuation = list(string.punctuation)
 stop = stopwords.words('french') + punctuation + ['via','le','les','a','rt'] # Liste des tokens à effacer
 
-stemmer = SnowballStemmer('french')
+stemmer1 = SnowballStemmer('french')
+stemmer2 = FrenchStemmer()
 
 count_stop = Counter() # Inisialise un compteur
-count_stem = Counter() # Inisialise un compteur
+count_stem1 = Counter() # Inisialise un compteur
+count_stem2 = Counter() # Inisialise un compteur
 for tweet in listOfTweets:
     try:
         tweetText = getTweetText(tweet)
@@ -97,23 +100,29 @@ for tweet in listOfTweets:
         tokens = preprocess(tweetText) # Tokenise le texte
         print('tokens')
         print(tokens)
-        terms_stem = [stemmer.stem(term) for term in tokens ]
+        terms_stem = [stemmer1.stem(term) for term in tokens ]
         print('stem sans stop')
         print(terms_stem)
         print(type(terms_stem))
         terms_stop = [term for term in tokens if term not in stop] # Crée une liste avec tout les termes sauf les termes stopé
-        terms_stem = [stemmer.stem(term) for term in terms_stop ]
-
-        print('stem avec stop')
-        print(terms_stem)
-        print(type(terms_stem))
+        terms_stem1 = [stemmer1.stem(term) for term in terms_stop ]
+        terms_stem2 = [stemmer2.stem(term) for term in terms_stop ]
+        print('stem1 avec stop')
+        print(terms_stem1)
+        print(type(terms_stem1))
+        print('stem2 avec stop')
+        print(terms_stem2)
+        print(type(terms_stem2))
         count_stop.update(terms_stop) # Met à jour le compteur avec les termes en parametres
-        count_stem.update(terms_stem) # Met à jour le compteur avec les termes en parametres
-        
+        count_stem1.update(terms_stem1) # Met à jour le compteur avec les termes en parametres
+        count_stem2.update(terms_stem2) # Met à jour le compteur avec les termes en parametres
+
     except:
         print('fail')
         pass
 print('stop most common')
 print(count_stop.most_common(40)) # Affiche les 20 mots les plus frequents
-print('stem most common')
-print(count_stem.most_common(40)) # Affiche les 20 mots les plus frequents
+print('stem1 most common')
+print(count_stem1.most_common(40)) # Affiche les 20 mots les plus frequents
+print('stem2 most common')
+print(count_stem2.most_common(40)) # Affiche les 20 mots les plus frequents
