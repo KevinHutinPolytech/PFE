941bb473c19bfd52820474ae9037798863aa7f6a
KevinHutinPolytech <hutinkevin@yahoo.fr>
2017-03-01 15:49:22 +0100

    Add files via upload

---
+++ test_pycon_2016.py
@@ -0,0 +1,27 @@
+import nltk
+
+text = "Bonjour, aujourd'hui semble être une belle journée"
+
+for sent in nltk.sent_tokenize(text):
+    print(list(nltk.pos_tag(nltk.word_tokenize(sent))))
+    print()
+    
+import json
+def read_file(file):
+        for line in file:
+            yield json.loads(line)["text"]
+
+from gensim import corpora
+with open ('stream_StopMacron.json', 'r') as f:
+        dictionary = corpora.Dictionary(line.strip().split() for line in read_file(f))
+
+
+class MyCorpus(object):
+    def __iter__(self):
+        for line in read_file(open ('stream_StopMacron.json', 'r')):
+            yield dictionary.doc2bow(line)
+
+from gensim import models
+lda = models.ldamodel.LdaModel(corpus = MyCorpus(), id2word = dictionary, num_topics = 100, update_every = 1, chunksize = 100000, passes = 3)
+print(lda)
+print(type(lda))
