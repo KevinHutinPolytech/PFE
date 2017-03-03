b9f2dbf6b4c5cc6dae304a80f94dd3152b085336
KevinHutinPolytech <hutinkevin@yahoo.fr>
2017-03-03 16:27:09 +0100

    Add files via upload

--- gensim_tuto.py
+++ gensim_tuto.py
@@ -58,3 +58,26 @@ else:
 model = models.LdaModel(corpus, id2word=dictionary, num_topics=100)
 
 pprint(model)
+
+#######################TUTO3###########################################
+from gensim import corpora, models, similarities
+dictionary = corpora.Dictionary.load('/tmp/deerwester.dict')
+corpus = corpora.MmCorpus('/tmp/deerwester.mm') # comes from the first tutorial, "From strings to vectors"
+print(corpus)
+
+lda = models.LdaModel(corpus, id2word=dictionary, num_topics=100)
+
+doc = "Human computer interaction"
+vec_bow = dictionary.doc2bow(doc.lower().split())
+vec_lsi = lsi[vec_bow] # convert the query to LSI space
+print(vec_lsi)
+
+index = similarities.MatrixSimilarity(lda[corpus]) # transform corpus to LSI space and index it
+
+index.save('/tmp/deerwester.index')
+index = similarities.MatrixSimilarity.load('/tmp/deerwester.index')
+
+sims = sorted(enumerate(sims), key=lambda item: -item[1])
+print(sims) # print sorted (document number, similarity score) 2-tuples
+
+
