d06850e74113d2996124e469acea149c9ee4cd93
KevinHutinPolytech <hutinkevin@yahoo.fr>
2017-03-07 15:48:17 +0100

    Add files via upload

---
+++ text2tokens.py
@@ -0,0 +1,68 @@
+# coding: utf8
+from __future__ import unicode_literals
+import operator 
+import json
+import sys
+from collections import Counter
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+import redis
+from nltk.corpus import stopwords
+import string
+from nltk import bigrams 
+from unidecode import unidecode
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.snowball import FrenchStemmer
+
+def tokenize(text):
+    return tokens_re.findall(text)
+ 
+def preprocess(text):
+    
+    emoticons_str = r"""
+        (?:
+            [:=;] # Eyes
+            [oO\-]? # Nose (optional)
+            [D\)\]\(\]/\\OpP] # Mouth
+        )"""
+     
+    regex_str = [
+        emoticons_str,
+        r'<[^>]+>', # HTML tags
+        r'(?:@[\w_]+)', # @-mentions
+        r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+        r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+     
+        r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+        r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+        r'(?:[\w_]+)', # other words
+        r'(?:\S)', # anything else
+        
+    ]
+        
+    tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+    emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+    """
+    The regular expressions are compiled with the flags re.VERBOSE, to allow spaces in the regexp to be ignored (see the multi-line emoticons regexp),
+    and re.IGNORECASE to catch both upper and lowercases. The tokenize() function simply catches all the tokens in a string and returns them as a list.
+    This function is used within preprocess(), which is used as a pre-processing chain: in this case we simply add a lowercasing feature for all the
+    tokens that are not emoticons (e.g. :D doesn’t become :d).
+    """
+    punctuation = list(string.punctuation)
+    stop = stopwords.words('french') + punctuation + ['via','le','les','a','rt'] # Liste des tokens à effacer
+
+    stemmer = SnowballStemmer('french')
+    tokens = tokenize(text)
+    tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    terms_stop = [term for term in tokens if term not in stop] # Crée une liste avec tout les termes sauf les termes stopé
+    terms_stem = [stemmer.stem(term) for term in terms_stop ]
+    return terms_stem
+
+filename = sys.argv[1]
+
+with open(filename,'r') as f:
+    for line in f:
+        list_of_tokens = preprocess(line)
+        
+print(list_of_tokens)
