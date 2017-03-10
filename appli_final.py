94af5e3b8f995c19a95d416f1778e60d687cd73b
KevinHutinPolytech <hutinkevin@yahoo.fr>
2017-03-10 18:12:58 +0100

    Update appli_final.py

--- appli_final.py
+++ appli_final.py
@@ -135,18 +135,18 @@ def txt2lda(monfichier):
             topicid = i[0]
             print(i[0])
             print(lda.get_topic_terms(topicid, topn=10))
-        # print(dictionary.token2id)
-        index = similarities.MatrixSimilarity(lda[corpus]) # transform corpus to LSI space and index it
+        index = similarities.MatrixSimilarity(lda[corpus]) # transform corpus to LDA space and index it
 
-        index.save('/tmp/deerwester.index')
-        index = similarities.MatrixSimilarity.load('/tmp/deerwester.index')
+        index.save('/tmp/emploi.index')
+        index = similarities.MatrixSimilarity.load('/tmp/emploi.index')
 
         sims = index[vec_lda]# perform a similarity query against the corpus
-        print(list(enumerate(sims))) # print (document_number, document_similarity) 2-tuples
+       # print(list(enumerate(sims))) # print (document_number, document_similarity) 2-tuples
         sims = sorted(enumerate(sims), key=lambda item: -item[1])
         print(sims) # print sorted (document number, similarity score) 2-tuples
+        print(sorted(dictionary.token2id))
         f.close()
-    return model
+    return lda
        
         
     
