400252811db3e50a8b4b2c489ee600a2571970eb
KevinHutinPolytech <hutinkevin@yahoo.fr>
2017-03-15 11:18:38 +0100

    Update test8.py

--- test8.py
+++ test8.py
@@ -14,7 +14,55 @@ from statistics import mode
 from nltk.tokenize import word_tokenize
 
 
+# Trait un texte en entrer (unicode) et retourne le texte tokeniser (mode = t),  stemmer (mode = s)
+def text2tokens(text,mode):
+    
+    emoticons_str = r"""
+        (?:
+            [:=;] # Eyes
+            [oO\-]? # Nose (optional)
+            [D\)\]\(\]/\\OpP] # Mouth
+        )"""
+     
+    regex_str = [
+        emoticons_str,
+        r'<[^>]+>', # HTML tags
+        r'(?:@[\w_]+)', # @-mentions
+        r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+        r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+     
+        r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+        r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+        r'(?:[\w_]+)', # other words
+        r'(?:\S)', # anything else
+        
+    ]
+        
+    tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+    emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+    """
+    The regular expressions are compiled with the flags re.VERBOSE, to allow spaces in the regexp to be ignored (see the multi-line emoticons regexp),
+    and re.IGNORECASE to catch both upper and lowercases. The tokenize() function simply catches all the tokens in a string and returns them as a list.
+    This function is used within preprocess(), which is used as a pre-processing chain: in this case we simply add a lowercasing feature for all the
+    tokens that are not emoticons (e.g. :D doesn’t become :d).
+    """
+    punctuation = list(string.punctuation)
+    stop = stopwords.words('french') + punctuation + ['>>','<<','<','>','via','le','les','a','rt'] # Liste des tokens à effacer
 
+    stemmer = SnowballStemmer('french')
+    try:
+        tokens = tokens_re.findall(unidecode(text))
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]    
+        terms_stop = [term for term in tokens if term not in stop] # Crée une liste avec tout les termes sauf les termes stopé
+        if mode == 't' :
+            return terms_stop
+        if mode == 's' :
+            terms_stem = [stemmer.stem(term) for term in terms_stop ]
+            return terms_stem
+    except:
+        print("Problème dans la tokenisation du text")
+        print("texte : ",text, "Type : ", type(text), "Mode : ",mode)
+        pass
 class VoteClassifier(ClassifierI):
     def __init__(self, *classifiers):
         self._classifiers = classifiers
@@ -36,7 +84,7 @@ class VoteClassifier(ClassifierI):
         conf = choice_votes / len(votes)
         return conf
     
-short_pos = open("Emploi.txt","r").read()
+
 
 # move this up here
 all_words = []
@@ -46,10 +94,14 @@ documents = []
 #  j is adject, r is adverb, and v is verb
 #allowed_word_types = ["J","R","V"]
 allowed_word_types = ["J"]
-
-for p in short_pos.split('\n').decode('unicode-escape'):
-    documents.append( (p, "pos") )
-    words = word_tokenize(p)
+with open("Emploi.txt",'r') as f:    
+    texts = [[tokens for tokens in text2tokens(line.decode('unicode-escape'),"t") if len(tokens) != 0 ] for line in f ]
+    # remove words that appear only once            
+    frequency = defaultdict(int)
+    for text in texts:
+        for token in text:
+            frequency[token] += 1
+    texts = [[token for token in text if frequency[token] > 1] for text in texts]
     pos = nltk.pos_tag(words)
     print("Words : " , words , "POS : " ,pos)
     for w in pos:
